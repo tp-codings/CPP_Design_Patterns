59b08da10200b7c3b2ac0ab2a61ae0323ef16596
tp-codings <tobipaul50@gmail.com>
2024-03-17 19:15:36 +0100

    basic test

--- Decorator/main.cpp
+++ Decorator/main.cpp
@@ -6,11 +6,21 @@
 int main()
 {
 
+    std::string data = "Much much much much data";
+
     DataSource* source = new FileDataSource("source.txt");
 
-    source->writeData("Much much much much data");
+    source->writeData(data);
 
+    source = new CompressionDecorator(source);
 
+    source->writeData(data);
+
+    source = new EncryptionDecorator(source);
+
+    source->writeData(data);
+
+    std::cout << source->readData();
 
     return 0;
 }
