49997a9d3ff7c68129ab0014137fe37b34d8a07f
tp-codings <tobipaul50@gmail.com>
2024-03-17 18:52:39 +0100

    basic test

--- Decorator/main.cpp
+++ Decorator/main.cpp
@@ -1,6 +1,16 @@
+#include "DataSource.h"
+#include "EncryptionDecorator.h"
+#include "CompressionDecorator.h"
+#include "FileDataSource.h"
 
 int main()
 {
+
+    DataSource* source = new FileDataSource("source.txt");
+
+    source->writeData("Much much much much data");
+    
+
 
     return 0;
 }
