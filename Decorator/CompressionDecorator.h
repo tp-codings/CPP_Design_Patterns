65280468938ca61988cdb6dad3bfea3aee7557c0
tp-codings <tobipaul50@gmail.com>
2024-03-17 18:46:12 +0100

    added CompressionDecorator.h

---
+++ Decorator/CompressionDecorator.h
@@ -0,0 +1,30 @@
+#ifndef COMPRESSIONDECORATOR_H
+#define COMPRESSIONDECORATOR_H
+
+#include "DataSourceDecorator.h"
+#include "DataSource.h"
+
+class CompressionDecorator : public DataSourceDecorator
+{
+
+    private:
+
+    public:
+        CompressionDecorator(DataSource* s):
+        DataSourceDecorator(s)
+        {
+
+        }
+
+        virtual void writeData(std::string data) override
+        {
+            DataSourceDecorator::_wrappee->writeData("compressed " + data);
+        }
+
+        virtual std::string readData() override
+        {
+            return "decompressed " + DataSourceDecorator::_wrappee->readData();
+        }
+};
+
+#endif
