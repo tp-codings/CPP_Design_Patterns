96abfea14d0a8ca6daee88fc07e26f8c86323bc5
tp-codings <tobipaul50@gmail.com>
2024-03-17 18:40:00 +0100

    added EncryptionDecorator.h

---
+++ Decorator/EncryptionDecorator.h
@@ -0,0 +1,38 @@
+#ifndef ENCRYPTIONDECORATOR_H
+#define ENCRYPTIONDECORATOR_H
+
+#include "DataSourceDecorator.h"
+#include "DataSource.h"
+
+class EncryptionDecorator : public DataSourceDecorator
+{
+    private:
+
+    public:
+        EncryptionDecorator(DataSource* s):
+        DataSourceDecorator(s)
+        {
+
+        }
+
+        virtual void writeData(std::string data) override
+        {
+            for(auto i : data)
+            {
+                i++;
+            }
+            DataSourceDecorator::_wrappee->writeData(data);
+        }
+
+        virtual std::string readData() override
+        {
+            std::string data = DataSourceDecorator::_wrappee->readData();
+
+            for(auto i : data)
+            {
+                i--;
+            }
+            return data;
+        }
+};
+#endif
